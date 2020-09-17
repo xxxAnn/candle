ba72df918a2e4fd81c685bad1328255a90a87f32
Kyando2 <amehikoji@gmail.com>
2020-09-17 16:30:06 -0400

    Modified test files

--- test.py
+++ test.py
@@ -7,11 +7,10 @@ logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s.%(funcName)s: %(message)s'
     )
 
-x = {3: [2, 3], 
-     4: {2: [2, 3, "8", "x", 3, 7, 1, {3: 4}]},  
-     2: {3: [1,2,3,4,5,6,7,8,9]}
-     } # replace by anything containing strings, ints, dicts and lists
-
+x = {"God": "rich",
+    "Satan": {"Sometimes": "broke", "Sometimes": "rich"},
+    "Me": [0, 0, 0 ,0]
+    }
 with open('tests/kyandle_file.kya', 'w') as f:
     f.write(Kyandle.serialize(x))
 
