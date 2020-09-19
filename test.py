a03645ac9182c9def9ab33f3d86a33a40fd7caa1
Kyando2 <amehikoji@gmail.com>
2020-09-20 01:41:54 +0900

    MODULEの名前を変更させていただきました

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import logging
 
-import Kyandle
+import きゃんどる
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s.%(funcName)s: %(message)s'
     )
 
-x = {"3": [2, (5, {3: "8"})]}
+これ = {"3": [2, (5, {3: "8"})]}
 with open('tests/kyandle_file.kya', 'w') as f:
-    f.write(Kyandle.serialize(x))
+    f.write(きゃんどる.serialize(これ))
 
 with open('tests/kyandle_file.kya', 'r') as f:
-    x = Kyandle.parse(f.read())
+    これ = きゃんどる.parse(f.read())
 
-print(x)
+print(これ)
